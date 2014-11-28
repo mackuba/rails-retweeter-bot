4dc706c613a16a7d2d47e86d23d07e98446a0c56
Jakub Suder <jakub.suder@gmail.com>
2014-11-28 14:43:27 +0100

    added updated deploy.rb to repo

---
+++ config/deploy.rb
@@ -0,0 +1,19 @@
+require 'bundler/capistrano'
+
+set :application, "rails_bot"
+set :repository, "git@github.com:jsuder/rails-retweeter-bot.git"
+set :scm, :git
+set :keep_releases, 5
+set :use_sudo, false
+set :deploy_to, "/var/www/rails_bot"
+set :deploy_via, :remote_cache
+
+server "matterhorn", :app, :web, :db, :primary => true
+
+after 'deploy:update_code', 'deploy:symlink_config'
+
+namespace :deploy do
+  task :symlink_config do
+    run "ln -s #{shared_path}/config/config.yml #{release_path}/config/config.yml"
+  end
+end
